1da74f3a19368106ccdd06706a16748a7e85af5f
Ahmed Mgua <mguaahmed@gmail.com>
2021-04-10 19:46:10 +0300

    Initial Commit

---
+++ JoodiApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JoodiApp
+//
+//  Created by Ahmed Mgua on 4/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JoodiApp/JoodiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoodiAppApp.swift
+//  JoodiApp
+//
+//  Created by Ahmed Mgua on 4/10/21.
+//
+
+import SwiftUI
+
+@main
+struct JoodiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
